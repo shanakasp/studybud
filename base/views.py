f4f5f76032bfeb60b3f16bd3e4a8c7d7a64fa60f
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-02 13:15:54 +0530

    changed 6 files

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'home.html')
+
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
--- studybud/settings.py
+++ studybud/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'base.apps.BaseConfig',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
