61f5cd3ca29bb2256886e77701b4a14d49bb430b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-02 21:19:53 +0530

    modified 6 files

--- base/views.py
+++ base/views.py
@@ -13,5 +13,11 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+        context = {'room': room}
+        return render(request, 'base/room.html', context)
