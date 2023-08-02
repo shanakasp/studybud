e3279877734903852886a9f1f4ec52f94fcbfb5b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-02 20:44:23 +0530

    modified 3 files

--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developer'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',
+                  {'rooms': rooms})
 
 
 def room(request):
